1b4e325cf8b83960848414d58bd41927a84297d5
mrmelectronics18 <prototypemrm2017@gmail.com>
2017-02-22 13:04:16 +0000

    PWM test working code

--- pwmTest.py
+++ pwmTest.py
@@ -2,14 +2,18 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3,GPIO.OUT)
-my_pwm = GPIO.(3,100)
+GPIO.setup(12,GPIO.OUT)
+my_pwm = GPIO.PWM(12,100000)
 my_pwm.start(0)
+try:
+	while(True):
+		for i in range(100):
+			my_pwm.ChangeDutyCycle(i)	
+			time.sleep(0.02)
+		for i in range(100):
+			my_pwm.ChangeDutyCycle(100-i)
+			time.sleep(0.02)
 
-while(True):
-	my_pwm.ChangeDutyCycle(100)
-	time.sleep(1)
-	my_pwm.ChangeDutyCycle(100)
-	time.sleep(1)
-	
-GPIO.cleanup()
+except KeyboardException:
+	my_pwm.stop()	
+	GPIO.cleanup()
